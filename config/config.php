12d6c8359f8b0d3df698c5c68d8ad0a7c7c68085
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-05-11 01:28:11 +0300

    adding db to heroku with phpmyadmin

--- config/config.php
+++ config/config.php
@@ -1,19 +1,29 @@
 <?php
 	session_start();
+	
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
 
-	// Define database
-	define('dbhost', 'localhost');
-	define('dbuser', 'root');
-	define('dbpass', '');
-	define('dbname', 'newrent');
+	// // Define database
+	// define('dbhost', 'localhost');
+	// define('dbuser', 'root');
+	// define('dbpass', '');
+	// define('dbname', 'heroku_f59da343fb0a9cc');
 
 	// Connecting database
-	try {
-		$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
-		$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch(PDOException $e) {
-		echo $e->getMessage();
-	}
+	// try {
+	// 	$connect = new PDO("mysql:host=".dbhost."; dbname=".dbname, dbuser, dbpass);
+	// 	$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// }
+	// catch(PDOException $e) {
+	// 	echo $e->getMessage();
+	// }
+	// Connect to DB
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 ?>
